d35fd440628816129e17473b7fc95ab9ac3691cb
Siddharth Udgata <siddharthu20@gmail.com>
2018-03-22 11:48:37 +0530

    Add files via upload

---
+++ StringOperation10.java
@@ -0,0 +1,28 @@
+package MyJavaPrograms_1;
+
+public class StringOperation10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//program to add "1" to each of the number in the string  and display the updated string in output
+		
+		String str = "4525";
+		
+		//output shud be 4525 + 1111 = 5636 
+		
+		
+	//	Integer i[]= new Integer[10]; 
+		
+		
+		Integer j = Integer.parseInt(str);
+		
+		Integer k = j + 1111;
+		
+	String res = k.toString();
+	
+	System.out.println("the resultant string is: "+res);
+	
+	}
+
+}
