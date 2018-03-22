5cd746662e09a8225bfa5da39208f4ae887846b0
Siddharth Udgata <siddharthu20@gmail.com>
2018-03-22 18:05:24 +0530

    Add files via upload

---
+++ ScannerClass.java
@@ -0,0 +1,28 @@
+package MyJavaPrograms_1;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		
+		//Using Scanner class
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		
+		
+		int res = i+j;
+		
+		System.out.println(res);
+		
+
+	}
+
+}
